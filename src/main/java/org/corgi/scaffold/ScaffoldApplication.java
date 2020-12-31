26077716ca2528b7174d1b479602f78e42fad929
郑明樟 <zhengmingzhang@meituan.com>
2021-01-01 07:31:17 +0800

    feat: init

---
+++ src/main/java/org/corgi/scaffold/ScaffoldApplication.java
@@ -0,0 +1,13 @@
+package org.corgi.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/corgi/scaffold/controller/DemoController.java
@@ -0,0 +1,12 @@
+package org.corgi.scaffold.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/")
+    public String demo(){
+        return "hello world";
+    }
+}
